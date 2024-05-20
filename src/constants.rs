f16770f9f58f5d4c54fd978af419e46cc499fc4b
dipdowel <leonid.bogouslavski@gmail.com>
2024-05-20 17:47:09 +0200

    POINT_ZERO added to the constants as it is an often-used point

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,4 @@
+use crate::graph1::primitives::primitives::Point;
 use crate::init::init_window::{WIN_HEIGHT, WIN_WIDTH};
 
 
@@ -9,3 +10,4 @@ pub const PIXEL_PER_PAGE: usize = (WIN_WIDTH * WIN_HEIGHT) as usize;
 /// How many full-screen pages are there in the screen buffer
 pub const NUM_OF_PAGES: usize = 4;
 pub const TOTAL_BUFFER_SIZE: usize = NUM_OF_PAGES * (WIN_WIDTH * WIN_HEIGHT) as usize;
+pub const POINT_ZERO: Point = Point { x: 0, y: 0 };
