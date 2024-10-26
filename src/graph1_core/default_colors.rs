60e7f29f05b2bb6831d3d2c6a0aff2c5decd6625
dipdowel <leonid.bogouslavski@gmail.com>
2024-10-26 15:12:30 +0200

    - Default colors for various purposes added

---
+++ src/graph1_core/default_colors.rs
@@ -0,0 +1,10 @@
+/// Default background color, RGBA
+pub const BACKGROUND: u32 = 0xff_ff_ff_ff;
+/// Default foreground color, RGBA
+pub const FOREGROUND: u32 = 0x11_22_33_ff;
+
+
+/// Default color for control points of Bézier curves
+pub const BEZIER_CONTROL: u32 = 0x00_00_33_ff;
+/// Default color for start and end points of Bézier curves
+pub const BEZIER_START_END: u32 = 0x00_ff_33_00;
