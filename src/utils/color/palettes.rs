02abc11f608c22347debce1dbfb0886fae282ce0
dipdowel <leonid.bogouslavski@gmail.com>
2024-10-27 01:24:44 +0200

    - Some stock color palettes added to be shipped with Graph1 by default

---
+++ src/utils/color/palettes.rs
@@ -0,0 +1,159 @@
+pub struct SunsetGlow;
+impl SunsetGlow {
+    pub const SOFT_ORANGE: u32 = 0xfa_8e_4b_ff;
+    pub const WARM_PEACH: u32 = 0xff_b3_75_ff;
+    pub const FADED_PINK: u32 = 0xff_a1_c0_ff;
+    pub const DUSTY_LAVENDER: u32 = 0xa3_88_bc_ff;
+    pub const EVENING_SKY_BLUE: u32 = 0x66_8f_bd_ff;
+    pub const GENTLE_INDIGO: u32 = 0x3d_5a_73_ff;
+    pub const DEEP_TEAL: u32 = 0x23_4e_52_ff;
+    pub const MELLOW_SLATE: u32 = 0x46_5d_6a_ff;
+    pub const DUSK_GRAY: u32 = 0x4a_4e_57_ff;
+    pub const PALE_MAUVE: u32 = 0xc6_b4_c8_ff;
+    pub const TWILIGHT_PURPLE: u32 = 0x5c_33_6b_ff;
+    pub const NIGHTFALL_BLUE: u32 = 0x1f_2a_4b_ff;
+}
+
+pub struct TropicalParadise;
+impl TropicalParadise {
+    pub const SAND_YELLOW: u32 = 0xf5_d7_98_ff;
+    pub const COCONUT_BROWN: u32 = 0x7a_4e_1d_ff;
+    pub const PALM_GREEN: u32 = 0x26_88_45_ff;
+    pub const LAGOON_BLUE: u32 = 0x36_98_bf_ff;
+    pub const CORAL_PINK: u32 = 0xff_82_88_ff;
+    pub const SEAFOAM_GREEN: u32 = 0x70_d1_ba_ff;
+    pub const SUNSET_CORAL: u32 = 0xff_8e_8f_ff;
+    pub const OCEAN_DEEP: u32 = 0x23_69_84_ff;
+    pub const MANGO_ORANGE: u32 = 0xff_ba_5a_ff;
+    pub const HIBISCUS_RED: u32 = 0xdc_47_50_ff;
+    pub const PAPAYA_YELLOW: u32 = 0xff_c1_4d_ff;
+    pub const COCONUT_WHITE: u32 = 0xfc_f6_e7_ff;
+}
+
+pub struct RetroNeon;
+impl RetroNeon {
+    pub const NEON_PINK: u32 = 0xff_00_99_ff;
+    pub const ELECTRIC_PURPLE: u32 = 0xb2_00_ff_ff;
+    pub const TURQUOISE_TEAL: u32 = 0x00_fa_ff_ff;
+    pub const VIBRANT_CYAN: u32 = 0x00_ff_e1_ff;
+    pub const LASER_LIME: u32 = 0xcc_ff_00_ff;
+    pub const MAGENTA_GLOW: u32 = 0xff_00_ff_ff;
+    pub const CYBER_BLUE: u32 = 0x00_9a_ff_ff;
+    pub const HOT_PINK: u32 = 0xff_57_93_ff;
+    pub const ELECTRIC_BLUE: u32 = 0x00_95_ff_ff;
+    pub const ACID_GREEN: u32 = 0x80_ff_00_ff;
+    pub const DARK_PURPLE: u32 = 0x56_00_7a_ff;
+    pub const VAPORWAVE_GRAY: u32 = 0x9a_9a_9a_ff;
+}
+
+pub struct ForestMist;
+impl ForestMist {
+    pub const PINE_GREEN: u32 = 0x29_62_4c_ff;
+    pub const FOREST_GREEN: u32 = 0x18_4a_2e_ff;
+    pub const MOSS_GREEN: u32 = 0x7d_8f_4e_ff;
+    pub const CEDAR_BROWN: u32 = 0x6a_4e_2d_ff;
+    pub const EARTHY_TAUPE: u32 = 0x8b_79_5e_ff;
+    pub const MIST_GRAY: u32 = 0xa8_b6_b5_ff;
+    pub const FOG_BLUE: u32 = 0x92_b1_bc_ff;
+    pub const SKY_GRAY: u32 = 0xcc_d3_c7_ff;
+    pub const SOFT_FERN: u32 = 0x98_af_75_ff;
+    pub const DEEP_BARK: u32 = 0x3d_33_2d_ff;
+    pub const MOSSY_ROCK: u32 = 0x76_81_5c_ff;
+    pub const WOODLAND_GREEN: u32 = 0x4e_6a_41_ff;
+}
+
+pub struct VintagePastel;
+impl VintagePastel {
+    pub const POWDER_PINK: u32 = 0xf3_b5_d0_ff;
+    pub const PALE_LAVENDER: u32 = 0xc6_a1_cd_ff;
+    pub const LIGHT_MINT: u32 = 0xa1_c7_a7_ff;
+    pub const CREAMY_YELLOW: u32 = 0xff_eb_a8_ff;
+    pub const BABY_BLUE: u32 = 0xb2_d1_e9_ff;
+    pub const SOFT_CORAL: u32 = 0xf7_ac_b6_ff;
+    pub const DUSTY_ROSE: u32 = 0xe8_9b_af_ff;
+    pub const MUTED_LAVENDER: u32 = 0xab_96_bd_ff;
+    pub const LIGHT_PEACH: u32 = 0xff_cd_b6_ff;
+    pub const PERIWINKLE: u32 = 0xa4_b9_e3_ff;
+    pub const SOFT_GRAY: u32 = 0xd1_d5_dd_ff;
+    pub const CLOUD_WHITE: u32 = 0xf5_f3_ef_ff;
+}
+
+pub struct DesertDusk;
+impl DesertDusk {
+    pub const SANDSTONE: u32 = 0xd9_b5_7b_ff;
+    pub const GOLDEN_SAND: u32 = 0xe1_c4_7a_ff;
+    pub const DESERT_ROSE: u32 = 0xee_a3_93_ff;
+    pub const RUSTY_ORANGE: u32 = 0xdc_7e_45_ff;
+    pub const EARTH_RED: u32 = 0x8c_4a_25_ff;
+    pub const SAGEBRUSH: u32 = 0xa3_ac_94_ff;
+    pub const CLAY_BROWN: u32 = 0xbd_80_52_ff;
+    pub const CACTUS_GREEN: u32 = 0x6f_7a_60_ff;
+    pub const SUNSET_BLUSH: u32 = 0xe0_8d_80_ff;
+    pub const WARM_OCHRE: u32 = 0xcf_9c_50_ff;
+    pub const FADED_PEACH: u32 = 0xec_c2_a4_ff;
+    pub const SHADOW_BROWN: u32 = 0x5c_4e_3b_ff;
+}
+
+pub struct WinterFrost;
+impl WinterFrost {
+    pub const FROST_BLUE: u32 = 0xc3_d9_ed_ff;
+    pub const SNOW_WHITE: u32 = 0xef_f9_fb_ff;
+    pub const ICE_GRAY: u32 = 0xdd_e6_f2_ff;
+    pub const PALE_LILAC: u32 = 0xc9_c7_e8_ff;
+    pub const COOL_BLUE: u32 = 0x91_a6_c9_ff;
+    pub const FROZEN_GREEN: u32 = 0x97_c1_bc_ff;
+    pub const FROSTED_LAVENDER: u32 = 0xb5_b4_e2_ff;
+    pub const SOFT_SILVER: u32 = 0xcc_d0_d9_ff;
+    pub const COLD_STEEL: u32 = 0x8a_9c_a4_ff;
+    pub const WINTER_MINT: u32 = 0x9d_c9_c2_ff;
+    pub const ICY_PINK: u32 = 0xf2_c6_cb_ff;
+    pub const ARCTIC_NAVY: u32 = 0x4c_5a_7a_ff;
+}
+
+pub struct AutumnHarvest;
+impl AutumnHarvest {
+    pub const PUMPKIN_ORANGE: u32 = 0xff_7d_4e_ff;
+    pub const MAPLE_RED: u32 = 0xe6_3b_3b_ff;
+    pub const BURNT_SIENNA: u32 = 0xd9_57_2c_ff;
+    pub const WARM_BEIGE: u32 = 0xcd_a8_7e_ff;
+    pub const HARVEST_GOLD: u32 = 0xff_d3_6e_ff;
+    pub const FOREST_GREEN: u32 = 0x42_8a_47_ff;
+    pub const CHESTNUT_BROWN: u32 = 0x7d_4e_2f_ff;
+    pub const MUSTARD_YELLOW: u32 = 0xff_c4_35_ff;
+    pub const OLIVE_GREEN: u32 = 0x6a_7d_33_ff;
+    pub const RUST_RED: u32 = 0xb5_32_30_ff;
+    pub const TAWNY: u32 = 0xdc_a8_66_ff;
+    pub const GOLDEN_ROD: u32 = 0xff_c1_5d_ff;
+}
+
+pub struct OceanBreeze;
+impl OceanBreeze {
+    pub const SEAFOAM: u32 = 0xa8_ef_dc_ff;
+    pub const AQUAMARINE: u32 = 0x75_d5_b3_ff;
+    pub const LAGOON_BLUE: u32 = 0x4e_a8_a1_ff;
+    pub const MARINE_BLUE: u32 = 0x38_6b_8e_ff;
+    pub const DEEP_SEA_BLUE: u32 = 0x1c_4e_75_ff;
+    pub const LIGHT_SAND: u32 = 0xf1_e8_ca_ff;
+    pub const CORAL_REEF: u32 = 0xff_a7_85_ff;
+    pub const DRIFTWOOD: u32 = 0xa3_8d_7b_ff;
+    pub const BRIGHT_CORAL: u32 = 0xff_8e_77_ff;
+    pub const DOLPHIN_GRAY: u32 = 0x9a_b3_bc_ff;
+    pub const FOAM_WHITE: u32 = 0xe6_f2_f1_ff;
+    pub const HARBOR_NAVY: u32 = 0x2b_3b_50_ff;
+}
+
+pub struct UrbanConcrete;
+impl UrbanConcrete {
+    pub const SLATE_GRAY: u32 = 0x6b_7c_85_ff;
+    pub const DARK_CHARCOAL: u32 = 0x36_3a_40_ff;
+    pub const ASH_GRAY: u32 = 0x99_99_99_ff;
+    pub const CLOUD_WHITE: u32 = 0xf7_f7_f7_ff;
+    pub const STEEL_BLUE: u32 = 0x41_5e_75_ff;
+    pub const IRON_GRAY: u32 = 0x42_4e_5e_ff;
+    pub const SHADOW_BLACK: u32 = 0x1a_1a_1a_ff;
+    pub const CONCRETE_GRAY: u32 = 0xb0_b7_c3_ff;
+    pub const CEMENT_BEIGE: u32 = 0xcd_c6_b3_ff;
+    pub const BRICK_RED: u32 = 0x8b_5a_55_ff;
+    pub const ASPHALT_GRAY: u32 = 0x2f_3c_44_ff;
+    pub const FOGGY_WHITE: u32 = 0xe8_e8_e8_ff;
+}
