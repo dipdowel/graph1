476951d9c699f062d2e753f35bea41779cb2d52a
dipdowel <leonid.bogouslavski@gmail.com>
2024-10-26 16:00:50 +0200

    - Added `Dimensions2d::new()` and `Dimensions2d::square()` - Added `RectArea::new()` and `RectArea::square()`

--- src/primitives/plane.rs
+++ src/primitives/plane.rs
@@ -20,6 +20,15 @@ impl<T: Numeric> Dimensions2d<T> {
             h: U::from_f64(self.h.to_f64()),
         }
     }
+    /// Creates a new `Dimensions2d` with the given `w` and `h` values.
+    pub fn new(w: T, h: T) -> Self {
+        Self { w, h }
+    }
+
+    /// Creates a new square `Dimensions2d`, with the given side length (i.e. w == h == side).
+    pub fn square(side: T) -> Self {
+        Self { w:side, h:side }
+    }
 }
 
 
@@ -28,5 +37,24 @@ impl<T: Numeric> Dimensions2d<T> {
 pub struct RectArea<T: Numeric = u32> {
     pub top_left: Point<T>,
     pub dimensions: Dimensions2d<T>,
+    pub color: Option<u32>,
 }
 
+impl<T: Numeric>  RectArea<T> {
+    pub fn new(x:T, y:T, w:T, h:T, color: Option<u32>) -> Self {
+        Self {
+            top_left: Point { x, y },
+            dimensions: Dimensions2d { w, h },
+            color,
+        }
+    }
+
+    pub fn square(x:T, y:T, side:T, color: Option<u32>) -> Self {
+        Self {
+            top_left: Point { x, y },
+            dimensions: Dimensions2d { w: side.clone(), h: side.clone() },
+            color,
+        }
+    }
+
+}
