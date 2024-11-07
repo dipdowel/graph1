7e0b385e0c26a331a02833c80abc68c346b45894
dipdowel <leonid.bogouslavski@gmail.com>
2024-11-07 22:39:29 +0100

    - `clear_screen()` added to utils

---
+++ src/utils/common.rs
@@ -0,0 +1,6 @@
+use crate::core::context::GraphContext;
+
+/// Fills the frame buffer with the background color of the window (`ctx.win.background_color`)
+pub fn clear_screen<UserDataType>(ctx: &mut GraphContext<UserDataType>) {
+    crate::draw::tools::fill::buffer(ctx.frame_buf, ctx.win.background_color);
+}
