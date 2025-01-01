1d240999706b8d537e9c270895745f462f861644
dipdowel <leonid.bogouslavski@gmail.com>
2025-01-01 03:31:37 +0100

    multithreading - Some refactoring and potential bug fix

--- src/utils/color/adapters/rgba_to_0rgb.rs
+++ src/utils/color/adapters/rgba_to_0rgb.rs
@@ -146,6 +146,8 @@ pub fn rgba_to_0rgb(
         blue: 0,
     }));
 
+    // Do nothing
+    //==============================================================================================
     if num_threads < 1 {
         if stats {
             let color_totals = *(color_totals.lock().unwrap());
@@ -154,15 +156,23 @@ pub fn rgba_to_0rgb(
         return None;
     }
 
+    // Execute in the main thread
+    //==============================================================================================
     if num_threads == 1 {
-        if stats {
-            buffer_rgba_to_0rgb_thread(dst, src, Some(color_totals.clone()));
-        } else {
+        // No statistics requested
+        if !stats {
             buffer_rgba_to_0rgb_thread(dst, src, None);
+            return None;
         }
+        // Prepare statistics
+        buffer_rgba_to_0rgb_thread(dst, src, Some(color_totals.clone()));
         let color_totals = *(color_totals.lock().unwrap());
         return Some(prepare_stats(color_totals, num_pixels));
     }
+
+    // Spawn multiple threads
+    //==============================================================================================
+
     // Size of the chunk for each thread to process
     let chunk_size = usize::div_ceil(src.len(), num_threads);
 
@@ -183,77 +193,12 @@ pub fn rgba_to_0rgb(
         }
     }); // The scope for the scoped threads ends here.
 
+
+    if !stats {
+        return None;
+    }
+
     let color_totals = *(color_totals.lock().unwrap());
-
     Some(prepare_stats(color_totals, num_pixels))
-
-    // // Calculate average color values
-    // let avg_r = (color_totals.red / num_pixels) as u32;
-    // let avg_g = (color_totals.green / num_pixels) as u32;
-    // let avg_b = (color_totals.blue / num_pixels) as u32;
-    //
-    // // Average ABGR color - using average alpha channel as 255 (opaque)
-    // let average_color = (avg_r << 24) | (avg_g << 16) | (avg_b << 8) | 0xff;
-    //
-    // Some(AdapterStatistics {
-    //     average_color,
-    //     average_red: avg_r & 0xff,
-    //     average_green: avg_g & 0xff,
-    //     average_blue: avg_b & 0xff,
-    //     num_pixels: num_pixels as u32,
-    // })
 }
 
-/*
-
-   // ===[ FASTER CONVERSION, NO STATISTICS ]======================================================
-   if !stats {
-       for (dst_pixel, &src_pixel) in dst.iter_mut().zip(src.iter()) {
-           // Extract individual color channels from RGBA
-           let r = (src_pixel >> 24) & 0xFF;
-           let g = (src_pixel >> 16) & 0xFF;
-           let b = (src_pixel >> 8) & 0xFF;
-
-           // Reassemble in ABGR format and store in dst
-           *dst_pixel = (0 << 24) | (r << 16) | (g << 8) | b;
-       }
-       return None;
-   }
-
-   // ===[ SLOWER CONVERSION, WITH THE STATISTICS ]================================================
-   let mut total_r = 0u64;
-   let mut total_g = 0u64;
-   let mut total_b = 0u64;
-   let num_pixels = (src.len() / 4) as u64;
-
-   for (dst_pixel, &src_pixel) in dst.iter_mut().zip(src.iter()) {
-       // Extract individual color channels from RGBA
-       let r = (src_pixel >> 24) & 0xFF;
-       let g = (src_pixel >> 16) & 0xFF;
-       let b = (src_pixel >> 8) & 0xFF;
-
-       // Reassemble in ABGR format and store in dst
-       *dst_pixel = (0 << 24) | (r << 16) | (g << 8) | b;
-
-       // Accumulate color values for statistics
-       total_r += r as u64;
-       total_g += g as u64;
-       total_b += b as u64;
-   }
-
-   // Calculate average color values
-   let avg_r = (total_r / num_pixels) as u32;
-   let avg_g = (total_g / num_pixels) as u32;
-   let avg_b = (total_b / num_pixels) as u32;
-
-   // Average ABGR color - using average alpha channel as 255 (opaque)
-   let average_color = (avg_r << 24) | (avg_g << 16) | (avg_b << 8) | 0xff;
-
-   Some(AdapterStatistics {
-       average_color,
-       average_red: avg_r & 0xff,
-       average_green: avg_g & 0xff,
-       average_blue: avg_b & 0xff,
-       num_pixels: num_pixels as u32,
-   })
-*/
