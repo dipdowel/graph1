9518665e3f727d0ade7b905bcbb33da758b7ee9c
dipdowel <leonid.bogouslavski@gmail.com>
2025-01-03 02:50:59 +0100

    multithreading - First attempt to multithread the rectangle

--- src/draw/rectangle.rs
+++ src/draw/rectangle.rs
@@ -3,6 +3,7 @@ use crate::core::context::GraphContext;
 use crate::primitives::plane::RectArea;
 use crate::utils::color::alpha::{blend_pixel_f32, blend_pixel_int};
 use std::cmp::PartialEq;
+use std::thread;
 
 impl PartialEq for AlphaMethod {
     fn eq(&self, other: &Self) -> bool {
@@ -53,52 +54,54 @@ fn draw_lines_of_rectangle_thread(
         }
     }
 
-    // for line in 0..total_lines {
-    //     for pixel in color_start..color_end {
-    //         index = line * line_length + pixel;
-    //
-    //         rectangle_slice[index] = color;
-    //
-    //         // match alpha_method {
-    //         //     AlphaMethod::None => {
-    //         //         rectangle_slice[index] = color;
-    //         //     }
-    //         //     AlphaMethod::Int => {
-    //         //         rectangle_slice[index] = blend_pixel_int(rectangle_slice[index], color);
-    //         //     }
-    //         //     AlphaMethod::Float => {
-    //         //         rectangle_slice[index] = blend_pixel_f32(rectangle_slice[index], color);
-    //         //     }
-    //         // }
-    //     }
-    // }
-
-    /*
-    // THis is a working solution, but it probably can be simplified
-    for line in 0..total_lines {
-        for pixel in 0..line_length {
-            let index = line * line_length + pixel;
-            if pixel >= color_start && pixel < color_end {
-                match alpha_method {
-                    AlphaMethod::None => {
-                        rectangle_slice[index] = color;
-                    }
-                    AlphaMethod::Int => {
-                        rectangle_slice[index] = blend_pixel_int(rectangle_slice[index], color);
-                    }
-                    AlphaMethod::Float => {
-                        rectangle_slice[index] = blend_pixel_f32(rectangle_slice[index], color);
-                    }
-                }
-            }
-        }
-    }
-    */
 }
 
 /// Draws a rectangle with dimensions and filled with a color specified in the `RectArea` struct.
 pub fn filled<UserData>(ctx: &mut GraphContext<UserData>, rect: &RectArea) {
+
+    // Do nothing...
+    if ctx.num_threads == 0 {
+        return;
+    }
+
+
     let color = rect.color.unwrap_or(ctx.win.foreground_color);
+
+    let alpha_method = if ctx.alpha.enabled {
+        ctx.alpha.method
+    } else {
+        AlphaMethod::None
+    };
+
+
+    // ==[ SINGLE THREAD ]==========================================================================
+    if ctx.num_threads == 1 {
+        // index of the first pixel in the first horizontal line of the rectangle
+        let first_line_start: usize = (rect.top_left.y * ctx.win.w) as usize;
+        // index of the last pixel in the last horizontal line of the rectangle
+        let last_line_end: usize = ((rect.top_left.y + rect.dimensions.h) * ctx.win.w) as usize;
+
+        let rectangle_slice = &mut ctx.frame_buf[first_line_start..last_line_end];
+        let line_length = ctx.win.w_usize;
+        let total_lines = rect.dimensions.h as usize;
+        let color_start = rect.top_left.x as usize;
+        let color_end = (rect.top_left.x + rect.dimensions.w) as usize;
+
+        draw_lines_of_rectangle_thread(
+            rectangle_slice,
+            line_length,
+            total_lines,
+            color_start,
+            color_end,
+            color,
+            alpha_method,
+        );
+
+        return;
+    }
+
+    // ==[ MULTIPLE THREADS ]=======================================================================
+
 
     // rect.dimensions.h /
     //
@@ -118,11 +121,6 @@ pub fn filled<UserData>(ctx: &mut GraphContext<UserData>, rect: &RectArea) {
 
     // TODO: 2. That way we can chunkify the buffer and process each chunk in a parallel thread
 
-    let alpha_method = if ctx.alpha.enabled {
-        ctx.alpha.method
-    } else {
-        AlphaMethod::None
-    };
 
     // index of the first pixel in the first horizontal line of the rectangle
     let first_line_start: usize = (rect.top_left.y * ctx.win.w) as usize;
@@ -131,53 +129,47 @@ pub fn filled<UserData>(ctx: &mut GraphContext<UserData>, rect: &RectArea) {
 
     let rectangle_slice = &mut ctx.frame_buf[first_line_start..last_line_end];
     let line_length = ctx.win.w_usize;
-    let total_lines = rect.dimensions.h as usize;
     let color_start = rect.top_left.x as usize;
     let color_end = (rect.top_left.x + rect.dimensions.w) as usize;
 
-    draw_lines_of_rectangle_thread(
-        rectangle_slice,
-        line_length,
-        total_lines,
-        color_start,
-        color_end,
-        color,
-        alpha_method,
-    );
+    // let mut total_lines = rect.dimensions.h as usize;
 
-    //
-    // for line in 0..total_lines {
-    //     for pixel in 0..line_length {
-    //         let index = line * line_length + pixel;
-    //         if pixel >= color_start && pixel < color_end {
-    //             rectangle_slice[index] = color;
-    //         }
-    //     }
-    // }
 
-    /*
-    // This is a working solution, but it requires the context, so let's leave it aside for now
-    for line in 0..rect.dimensions.h {
-        let start_index = (rect.top_left.y + line) * ctx.win.w + rect.top_left.x;
-        for column in 0..rect.dimensions.w {
-            let index = (start_index + column) as usize;
+    let chunk_size = usize::div_ceil(rectangle_slice.len(), ctx.num_threads);
+    let mut chunks: Vec<&mut [u32]> = rectangle_slice.chunks_mut(chunk_size).collect();
 
-            // No alpha enabled, just assign the color
-            if !ctx.alpha.enabled {
-                ctx.frame_buf[index ] = color;
-                continue;
-            }
-            // Alpha Int
-            if ctx.alpha.method == AlphaMethod::Int {
-                ctx.frame_buf[index ] = blend_pixel_int(ctx.frame_buf[index ], color);
-            }
-            // Alpha Float
-            if ctx.alpha.method == AlphaMethod::Float {
-                ctx.frame_buf[index ] = blend_pixel_f32(ctx.frame_buf[index ], color);
-            }
+    // FIXME:
+    // FIXME: There's a bug: when the number of threads cannot be divided by 2 or 4 or so on,
+    // FIXME: the rectangle breaks
+    // FIXME:
+    // FIXME:
+
+    thread::scope(|s| {
+        // Iterate over the chunks and process each in its own thread
+        for  chunk in chunks.iter_mut() {
+            // `chunk_index + 1` since the main thread is already processing the 0-th chunk
+
+            s.spawn(move ||
+                        draw_lines_of_rectangle_thread(
+                            chunk,
+                            line_length,
+                            chunk.len() / line_length,
+                            color_start,
+                            color_end,
+                            color,
+                            alpha_method,
+                        )
+
+            );
+
         }
-    }
-    */
+    }); // The scope for the scoped threads ends here. All the threads are expected to be joined automagically at this point.
+
+
+
+
+
+
 
     // ==[ MULTIPLE THREADS ]=======================================================================
 
@@ -213,58 +205,4 @@ pub fn filled<UserData>(ctx: &mut GraphContext<UserData>, rect: &RectArea) {
         }
     */
 
-    //take a mutable sub-slice of the
-
-    /*
-
-
-    // Dereference the options
-    let start_x = rect.top_left.x;
-    let start_y = rect.top_left.y;
-    let width = rect.dimensions.w;
-    let height = rect.dimensions.h;
-
-    // Nothing to draw here
-    if width == 0 || height == 0 {
-        return;
-    }
-
-    let end_x = start_x + width;
-    let end_y = start_y + height;
-
-    let mut x = start_x;
-    let mut y = start_y;
-
-    // Which pixel in the vector should be filled in next.
-    let mut pixel_index: usize;
-
-    let mut resulting_color: u32 = color;
-
-    loop {
-        pixel_index = (y * ctx.win.w + x) as usize;
-
-        // If alpha blending is enabled, blend the new color with the existing pixel color
-        // according to the alpha channel.
-        if ctx.alpha.enabled {
-            if ctx.alpha.method == AlphaMethod::Int {
-                resulting_color = blend_pixel_int(ctx.frame_buf[pixel_index], color);
-            }
-            if ctx.alpha.method == AlphaMethod::Float {
-                resulting_color = blend_pixel_f32(ctx.frame_buf[pixel_index], color);
-            }
-        }
-        ctx.frame_buf[pixel_index] = resulting_color;
-
-        x += 1;
-
-        if x == end_x || x == ctx.win.w {
-            y += 1;
-            x = start_x;
-        };
-
-        if y == end_y || y == ctx.win.h {
-            break;
-        }
-    }
-    */
 }
