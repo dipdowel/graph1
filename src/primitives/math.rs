045fba027493314ad93ba332ee59db71166ed7f6
dipdowel <leonid.bogouslavski@gmail.com>
2025-03-15 19:53:29 +0100

    - math primitives updated

--- src/primitives/math.rs
+++ src/primitives/math.rs
@@ -1,6 +1,7 @@
 use crate::primitives::numeric::Numeric;
 
-
+/// A range of values.
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Range<T: Numeric> {
     pub start: T,
     pub end: T,    
@@ -14,6 +15,9 @@ impl <T: Numeric> Range<T> {
 
 
 
+/// A structure that holds the minimum and maximum values of a type.
+///
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct MinMax<T: Numeric> {
     pub min: T,
     pub max: T,
@@ -23,3 +27,9 @@ impl <T: Numeric> MinMax<T> {
         Self { min, max }
     }
 }
+
+pub const MIN_MAX_U64: MinMax<u64> = MinMax { min: 0, max: u64::MAX };
+pub const MIN_MAX_U32: MinMax<u32> = MinMax { min: 0, max: u32::MAX };
+pub const MIN_MAX_I32: MinMax<i32> = MinMax { min: i32::MIN, max: i32::MAX };
+pub const MIN_MAX_F32: MinMax<f32> = MinMax { min: f32::MIN, max: f32::MAX };
+pub const MIN_MAX_F64: MinMax<f64> = MinMax { min: f64::MIN, max: f64::MAX };
