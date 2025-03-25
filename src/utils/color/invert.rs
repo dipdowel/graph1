0d05d738b9b3c5ee4bb3b38d41f52bb1a8fe623d
dipdowel <leonid.bogouslavski@gmail.com>
2025-03-26 00:40:57 +0100

    - `invert` refactored!

--- src/utils/color/invert.rs
+++ src/utils/color/invert.rs
@@ -1,5 +1,8 @@
 use crate::primitives::plane::{Dimensions2d, RectArea};
 use crate::primitives::point::Point;
+use std::cmp::min;
+
+
 
 /// # Arguments
 /// * `buf` - A mutable slice of u32 values representing the pixel buffer. Each pixel is a 0RGB value.
@@ -12,22 +15,16 @@ pub fn invert_colors(buf: &mut [u32], buf_dimensions: &Dimensions2d, area: &Rect
 
     //----------------------------------------------------------------------------------------------
     // ENSURE COORDINATES ARE WITHIN THE BOUNDS OF THE BUFFER
-    let rightmost_x = area.top_left.x + area.dimensions.w;
-    let bottommost_y = area.top_left.y + area.dimensions.h;
 
-    let dst_area_w = if rightmost_x > buf_dimensions.w {
-        println!("Rightmost x coordinate exceeds buffer width, clamping to buffer width");
-        buf_dimensions.w - area.top_left.x
-    } else {
-        area.dimensions.w
-    };
+    let dst_area_w = min(
+        area.dimensions.w,
+        buf_dimensions.w.saturating_sub(dst_point.x),
+    );
+    let dst_area_h = min(
+        area.dimensions.h,
+        buf_dimensions.h.saturating_sub(dst_point.y),
+    );
 
-    let dst_area_h = if bottommost_y > buf_dimensions.h {
-        println!("Bottommost y coordinate exceeds buffer height, clamping to buffer height");
-        buf_dimensions.h - area.top_left.y
-    } else {
-        area.dimensions.h
-    };
     //----------------------------------------------------------------------------------------------
 
     // Iterate over each pixel in the specified source region
@@ -50,18 +47,13 @@ pub fn invert_colors(buf: &mut [u32], buf_dimensions: &Dimensions2d, area: &Rect
 }
 
 /// Inverts the color of an RGBA pixel.
+#[inline(always)]
+
 pub fn invert_pixel(pixel: u32) -> u32 {
     // Color as channels
     let r = (pixel >> 24) & 0xFF;
     let g = (pixel >> 16) & 0xFF;
     let b = (pixel >> 8) & 0xFF;
     let a = pixel & 0xFF;
-
-    // Compute the opposite color components
-    let r_opposite = 0xff - r;
-    let g_opposite = 0xff - g;
-    let b_opposite = 0xff - b;
-
-    // Combine the components back into an RGBA color
-    (r_opposite << 24) | (g_opposite << 16) | (b_opposite << 8) | a
+    ((0xFF - r) << 24) | ((0xFF - g) << 16) | ((0xFF - b) << 8) | a
 }
