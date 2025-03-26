8c33dedb03232639ff948cee95cd7c035fff971d
dipdowel <leonid.bogouslavski@gmail.com>
2025-03-26 15:22:54 +0100

    - Tests added

--- src/utils/math/gcd.rs
+++ src/utils/math/gcd.rs
@@ -9,3 +9,30 @@ pub fn gcd<T: Numeric>(mut a: T, mut b: T) -> T {
     }
     a
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gcd_basic_cases() {
+        assert_eq!(gcd(48, 18), 6);
+        assert_eq!(gcd(101, 103), 1);
+        assert_eq!(gcd(56, 98), 14);
+        assert_eq!(gcd(17, 0), 17);
+        assert_eq!(gcd(0, 25), 25);
+        assert_eq!(gcd(0, 0), 0);
+    }
+
+    #[test]
+    fn test_gcd_same_numbers() {
+        assert_eq!(gcd(7, 7), 7);
+        assert_eq!(gcd(123456, 123456), 123456);
+    }
+
+    #[test]
+    fn test_gcd_large_numbers() {
+        assert_eq!(gcd(1_000_000_007_u64, 123456789_u64), 1);
+        assert_eq!(gcd(4294967296_u64, 65536_u64), 65536);
+    }
+}
