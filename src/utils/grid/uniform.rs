5fb9acaa28017fdc72b4ae651ec1a01245386342
dipdowel <leonid.bogouslavski@gmail.com>
2025-05-17 18:46:31 +0200

    - simple rendering of the grid added - support for coloring each cell added

--- src/utils/grid/uniform.rs
+++ src/utils/grid/uniform.rs
@@ -1,3 +1,5 @@
+use crate::core::context::GraphContext;
+use crate::draw;
 use crate::primitives::numeric::Numeric;
 use crate::primitives::plane::{Dimensions2d, RectArea};
 use crate::utils::math::geometry::region::Region;
@@ -20,11 +22,15 @@ impl<T: Numeric> UniformGrid<T> {
     /// Constructs a new grid based on the provided props
     ///
     /// # Parameters
-    /// - `proto_cell`: This `RectArea` is used to construct the grid cells.
-    /// The top-left corner of the grid corresponds to the top-left corner of this `RectArea`.
-    /// The default color of the grid cells is the same as the color of this `RectArea`.
+    /// - `proto_cell`: use as a template for a grid cell.
+    ///     - The top-left corner of `proto_cell` is used as the top-left corner for the whole grid
+    ///     - The default color of `proto_cell` is assigned to each created cell (can be changed later)
     /// - `rows`: Number of rows in the grid
     /// - `cols`: Number of columns in the grid
+    /// - `colors`: Optional vector of colors for the cells. It is treated as a ring buffer, 
+    /// the first color from `colors` is applied to the first cell, the second color to the second cell, and so on.
+    /// When the end of the vector is reached, the color pointer is reset to the beginning of the vector and the coloring
+    /// continues from there.
     ///
     /// # Returns
     /// A new `UniformGrid` instance with the specified number of rows and columns.
@@ -32,6 +38,7 @@ impl<T: Numeric> UniformGrid<T> {
         proto_cell: RectArea<T>,
         rows: usize,
         cols: usize,
+        colors:Option<Vec<u32>>,
     ) -> Self  {
         let mut cells = Vec::with_capacity(rows * cols);
         let w = proto_cell.dimensions.w;
@@ -39,11 +46,22 @@ impl<T: Numeric> UniformGrid<T> {
         let base_x = proto_cell.top_left.x;
         let base_y = proto_cell.top_left.y;
 
+
+        let colors:Vec<Option<u32>> = colors
+            .map(|v| v.into_iter().map(Some).collect())
+            .unwrap_or(vec![proto_cell.color]);
+
+        // Create an infinite circular iterator over `result`
+        let mut circular_iter = colors.iter().cycle();
+
         for row in 0..rows {
             for col in 0..cols {
                 let x = base_x + T::from_u32(col as u32) * w; // FIXME! will break on huge grids
                 let y = base_y + T::from_u32(row as u32) * h; // FIXME! will break on huge grids
-                let rect = RectArea::new(x, y, w, h, proto_cell.color);
+                // let rect = RectArea::new(x, y, w, h, circular_iter.next().unwrap_or(proto_cell.color));
+                let color = *circular_iter.next().unwrap_or(&proto_cell.color);
+
+                let rect = RectArea::new(x, y, w, h, color);
                 cells.push(Region::new(rect));
             }
         }
@@ -143,5 +161,36 @@ impl<T: Numeric> UniformGrid<T> {
     }
     pub fn num_cells(&self) -> usize {
         self.rows * self.cols
+    }
+}
+
+
+
+/// Renders a `UniformGrid` instance on the screen.
+/// NB: This method is very basic and is mostly meant for visual debugging purposes.
+/// NB: It is recommended to implement your own rendering if you need something more complex.
+///
+/// # Parameters
+/// - `ctx`: A mutable reference to the drawing context.
+/// - `grid`: A `UniformGrid` instance representing the grid to be rendered.
+/// - `use_outline`: A boolean flag indicating whether to draw the grid cells as outlines or filled rectangles. Color for each cell is taken from its internal `RectArea`.
+
+pub fn render<UserData, T: Numeric>(
+    ctx: &mut GraphContext<UserData>,
+    grid: &UniformGrid<T>,
+    use_outline: bool,
+) {
+    if use_outline {
+        grid.iter().for_each(|cell_t| {
+            let cell: Region<u32> = cell_t.convert();
+            let rect_area: RectArea<u32> = cell.rect_area();
+            draw::rectangle::outline(ctx, &rect_area);
+        });
+    } else {
+        grid.iter().for_each(|cell_t| {
+            let cell: Region<u32> = cell_t.convert();
+            let rect_area: RectArea<u32> = cell.rect_area();
+            draw::rectangle::filled(ctx, &rect_area);
+        });
     }
 }
