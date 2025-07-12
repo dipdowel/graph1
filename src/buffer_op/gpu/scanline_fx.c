0f597325c93ea351444546480e670fe0c8d3c5c1
dipdowel <leonid.bogouslavski@gmail.com>
2025-07-12 23:53:54 +0200

    - The `scanline_fx` kernel cleaned up

--- src/buffer_op/gpu/scanline_fx.c
+++ src/buffer_op/gpu/scanline_fx.c
@@ -1,27 +1,3 @@
-//__kernel void scanline_fx(
-//    __global uint *buf,
-//    uint intensity,
-//    uint line_flipper,
-//    uint pixel_count,
-//    uint width
-//) {
-//    uint gid = get_global_id(0);
-//    if (gid >= pixel_count) return;
-//
-//    // Figure out if this pixel is in a scanline "row"
-//    if (((gid / line_flipper) % 2) == 0) {
-//        // Do subtract intensity (on packed RGBA)
-//        uint c = buf[gid];
-//
-//        // Subtract the intensity from each color channel except alpha
-//
-//        // Fast path: subtract as a 32-bit value (may overflow/underflow)
-//        buf[gid] = c - intensity;
-//    }
-//}
-
-
-
 __kernel void scanline_fx(
     __global uint *buf,
     uint intensity,
